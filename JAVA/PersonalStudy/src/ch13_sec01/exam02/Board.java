26cf1e21deef889c5b4e5bc47ea35df9e328a284
Dayeong <noda21@naver.com>
2022-09-22 21:08:40 +0900

    study

---
+++ JAVA/PersonalStudy/src/ch13_sec01/exam02/Board.java
@@ -0,0 +1,14 @@
+package ch13_sec01.exam02;
+
+public class Board {
+	//field
+	String title;
+	String content;
+	String author;
+
+	public Board(String title, String content, String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;	
+	}
+}
