78a8e5f612f21c0eb836e90d2f14429cdfa727b5
Dayeong <noda21@naver.com>
2022-10-24 10:18:04 +0900

    mypagefinal

--- WebBasic_Script/main/webapp/Squadfree/assets/js/main.js
+++ WebBasic_Script/main/webapp/Squadfree/assets/js/main.js
@@ -259,3 +259,21 @@
   new PureCounter();
 
 })()
+
+
+/*google map*/
+var map;
+
+function initMap() {
+  var seoul = { lat: 37.5642135 ,lng: 127.0016985 };
+  map = new google.maps.Map( document.getElementById('map'), {
+      zoom: 12,
+      center: seoul
+    });
+
+  new google.maps.Marker({
+    position: seoul,
+    map: map,
+    label: "Dayeong is here!"
+  });
+}
